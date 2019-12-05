be62a1030a4df2ec88c02fee3368af56eb35597b
Cedric Priestley <cedricpriestley@gmail.com>
2019-12-05 00:49:38 -0500

    remove code to clone scripts repo since scripts has been added ro this repo

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,12 +4,12 @@ pkg up -y
 pkg i git zsh -y
 pkg i bat fzf git nnn tmux vim -y
   
+cd $HOME
+
 git config --global user.name "Cedric Priestley"
 git config --global user.email cedricpriestley@gmail.com
 git config --global credential.helper store
  
-cd $HOME
-
 git clone https://github.com/cedricpriestley/linux-dotfiles.git dotfiles
 ln -s $HOME/dotfiles/.termux $HOME/.termux
 ln -s $HOME/dotfiles/.vim $HOME/.vim
@@ -18,7 +18,6 @@ echo "source $HOME/dotfiles/aliases.sh" >> $HOME/.zshr
  
 echo "source $HOME/dotfiles/envars.sh" >> $HOME/.zshr
  
-git clone https://github.com/cedricpriestley/linux-scripts.git scripts
-chmod u+x ./scripts/install.sh
+chmod u+x $HOME/dotfiles//scripts/install.sh
  
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
