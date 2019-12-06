2dfb1b2aea62bbe90a1d95c8a0f05d5a3e467f48
Cedric Priestley <cedricpriestley@gmail.com>
2019-12-06 03:30:41 -0500

    combine  init and install scripts. add install for zsh-autosuggestions and z-zsh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
-pkg i man aria2
-pkg i nnn -y
-pkg i tmux -y
-pkg i vim -y
+pkg up -y
 
+pkg i bat fzf git htop nnn termux-api tmux vim zsh -y 
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+
+git clone https://github.com/cedricpriestley/linux-dotfiles.git $HOME/dotfiles
+
+ln -s $HOME/dotfiles/.termux $HOME/.termux
+ln -s $HOME/dotfiles/.vim $HOME/.vim
+
+echo "zstyle ':completion:*' menu select" >> $HOME/.zshr
+c
+echo "source $HOME/dotfiles/aliases.sh" >> $HOME/.zshrc
+ 
+echo "source $HOME/dotfiles/envars.sh" >> $HOME/.zshrc
+ 
+git config --global user.name "Cedric Priestley"
+git config --global user.email cedricpriestley@gmail.com
+git config --global credential.helper store
