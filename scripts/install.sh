c1dfdd87b2966fd6d7d6c821550fa9e5c538684e
cedricpriestley <cedricpriestley@gmail.com>
2019-12-06 05:40:18 -0500

    Fix incompatible directory move

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,7 +12,7 @@ git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 
 git clone https://github.com/cedricpriestley/linux-dotfiles.git $HOME/dotfiles
 
-mv $HOME/.termux/*(DN) $HOME/dotfiles/.termux/
+mv $HOME/.termux/* $HOME/dotfiles/.termux/
 rm $HOME/.termux
 ln -s $HOME/dotfiles/.termux $HOME/.termux
 ln -s $HOME/dotfiles/.vim $HOME/.vim
