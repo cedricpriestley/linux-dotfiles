f2fc753af6632844beb262b02847a79d3f32b5d3
Cedric Priestley <cedricpriestley@gmail.com>
2019-12-09 04:22:48 +0000

    add aliases. add symlinks

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 pkg up -y
 
-pkg i bat fzf git htop nnn termux-api tmux vim zsh -y 
+pkg i bat fzf git nodejs-lts silversearcher-ag man proot nnn termux-api tmux vim zsh -y 
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -16,9 +16,11 @@ mv $HOME/.termux/* $HOME/dotfiles/.termux/
 rm -fr $HOME/.termux
 ln -s $HOME/dotfiles/.termux $HOME/.termux
 ln -s $HOME/dotfiles/.vim $HOME/.vim
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 
-echo "zstyle ':completion:*' menu select" >> $HOME/.zshr
-c
+echo "zstyle ':completion:*' menu select" >> $HOME/.zshrc
+
 echo "source $HOME/dotfiles/aliases.sh" >> $HOME/.zshrc
  
 echo "source $HOME/dotfiles/envars.sh" >> $HOME/.zshrc
