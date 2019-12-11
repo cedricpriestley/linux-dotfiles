a0bfc082b44c16e5e881de9d77fcc494b96d6919
cedricpriestley <cedricpriestley@gmail.com>
2019-12-11 15:54:35 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,15 +2,27 @@
 
 pkg up -y
 
-pkg i bat fzf git nodejs-lts silversearcher-ag man proot nnn termux-api tmux vim nano openssh ripgrep -y 
+# pkg i bat fzf git nodejs-lts silversearcher-ag man proot nnn termux-api tmux vim nano openssh ripgrep -y 
 
+pkg i autotools curl zlib openssl expat libiconv
+# git build requirements - autotools, curl, zlib, openssl, expat, and libiconv
+curl -o git-2.24.1.tar.gz https://github.com/git/git/archive/v2.24.1.tar.gz
+tar -zxf git-2.24.1.tar.gz
+cd git-2.24.1
+make configure
+./configure --prefix=/usr
+# make all doc info
+make install install-doc install-html install-info
+
+# Clone the repo.containing the init & install scripts and default dotfile configuration
 git clone https://github.com/cedricpriestley/linux-dotfiles.git $HOME/dotfiles
-
-cp -r $HOME/dotfiles/.termux/* $HOME
 
 ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+
+# Termux-specific configuration
+ln -s $HOME/dotfiles/.termux $HOME/.termux
  
 git config --global user.name "Cedric Priestley"
 git config --global user.email cedricpriestley@gmail.com
